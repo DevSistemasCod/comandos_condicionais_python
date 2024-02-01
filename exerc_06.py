bc8b8c47ec00a0c318f2b3164e559a8c7a1ec617
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2024-02-01 15:21:44 -0300

    Add files via upload

--- exerc_06.py
+++ exerc_06.py
@@ -4,25 +4,36 @@ IDADE_MIN_MULHER = 58
 TEMPO_SERV_HOMEM = 30
 TEMPO_SERV_MULHER = 35
 
-genero = input("Qual o seu gênero")
+genero = input("Qual o seu gênero: ").lower()
+genero 
 
 if(genero == "h"):
-    idade = input("Informe a sua idade:")
+    idade = int(input("Informe a sua idade: "))
     if(idade >= IDADE_MIN_HOMEM ):
-        print("Pode Aposentar por idade")
-    
-    tempo_de_servico =  input("Informe o tempo de seriço:")
+        print("Pode se aposentar por idade")
+    else:
+        print("Não pode se aposentar por idade")
+
+    tempo_de_servico =  int(input("Informe o tempo de serviço: "))
     if(tempo_de_servico >= TEMPO_SERV_HOMEM):
-        print("Pode Aposentar por tempo de serviço")
+        print("Pode se aposentar por tempo de serviço")
+    else:
+        print("Não pode se aposentar por tempo de serviço")
+
 
 elif(genero == "m"):
-    idade = input("Informe a sua idade:")
+    idade = int(input("Informe a sua idade: "))
     if(idade >= IDADE_MIN_MULHER ):
-        print("Pode Aposentar por idade")
-    
-    tempo_de_servico =  input("Informe o tempo de seriço:")
+        print("Pode se aposentar por idade")
+    else:
+        print("Não pode se aposentar por idade")
+
+    tempo_de_servico =  int(input("Informe o tempo de serviço: "))
     if(tempo_de_servico >= TEMPO_SERV_MULHER):
-        print("Pode Aposentar por tempo de serviço")
+        print("Pode se aposentar por tempo de serviço")
+    else:
+        print("Não pode se aposentar por tempo de serviço")
+
 
 else:
     print("As entradas aceitas são h ou m")
